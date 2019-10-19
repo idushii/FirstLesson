2cdd5c9cd9582c17ae980e510367a690bd3e82d9
oleggolen <oleg-golenischev63@yandex.ru>
2019-10-19 13:44:38 +0700

    добавлена теория

--- FirstLessonApp/FirstLessonApp.cpp
+++ FirstLessonApp/FirstLessonApp.cpp
@@ -5,8 +5,18 @@
 using namespace std; //чтобы не писать std::
 int main()
 {
-    cout << "Hello World!\n"; 
-	cout << "print text"; //вывод текста на экран
-	cout << "hello 2";
+	int a = 10;
+	int c;
+	bool b = false;
+	double d = 3.2e-5; // 3.2*10^-5
+	cin >> a >> c;
+	cout << a << " " << c << endl;
+	int s = a + c;
+	cout << s << endl;
+	cout << a <<"\n"; // \n - переход на новую строку
+	cout << a << " " << b << " " << d <<endl; // endl - переход на новую строку
+    cout << "Hello World!" << endl; 
+	cout << "print text\n"; //вывод текста на экран
+	cout << "hello 2" << "\n";
 	return 0;
 }
