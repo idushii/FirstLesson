e89f775ae2fc03268cf4fbd945d96fccd5f4152c
oleggolen <oleg-golenischev63@yandex.ru>
2019-10-19 11:32:15 +0700

    сделан первый урок по C++

---
+++ FirstLessonApp/FirstLessonApp.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream> //ввод, вывод
+/*#include<cmath> //математические функции
+#include<ctime>//работа со временем
+*/
+using namespace std; //чтобы не писать std::
+int main()
+{
+    cout << "Hello World!\n"; 
+	cout << "print text"; //вывод текста на экран
+	return 0;
+}
